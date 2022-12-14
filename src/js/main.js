e7ed1579be93cf58e1b21ce92222ef35783a8694
Aleksandr <krasa19920906@gmail.com>
2022-12-14 15:51:33 +0300

    created a class for dynamically displaying cards

--- src/js/main.js
+++ src/js/main.js
@@ -141,6 +141,71 @@ window.addEventListener("DOMContentLoaded",()=>{
     }
 
     window.addEventListener("scroll",showModalByScroll);
+
+    // Импользуем классы для карточек меню
+    class MenuCard {
+        constructor (srcImg,alt,title,descr,price,parentSelector) {
+            this.srcImg = srcImg;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.parent = document.querySelector(parentSelector)// куда будем вставлять карточки (какой то элемент страницы)
+            this.price = price;
+            this.transfer = 27;// создаем св-во по умолчанию курс$
+            this.changeToUah();// запускаем функ конвертации и записываем результат в price
+        }
+
+        changeToUah () {
+           return this.price = this.price * this.transfer
+        }
+
+        // Создаем метод для отображения карточек на странице
+
+        render() {
+            const element = document.createElement("div");// Создаем элемент внутрь которого поместим разметку
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src="${this.srcImg}" alt="${this.alt}">
+                    <h3 class="menu__item-subtitle">Меню ${this.title}</h3>
+                    <div class="menu__item-descr">Меню ${this.title} ${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+
+            this.parent.append(element)
+        }
+    }
+
+    // const div = new MenuCard(); один из способов
+    // div.render();
+
+    new MenuCard("img/tabs/vegy.jpg",
+    "vegy",
+    "Фитнес", 
+    "- это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+    9,
+    ".menu .container"
+    ).render();
+
+    new MenuCard("img/tabs/elite.jpg",
+    "elite",
+    "Премиум", 
+    "- мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+    21,
+    ".menu .container"
+    ).render();
+
+    new MenuCard("img/tabs/post.jpg",
+    "post",
+    "Постное", 
+    "- это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+    14,
+    ".menu .container"
+    ).render();
 });
 
 
